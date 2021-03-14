5a34a3cd0dd493b13f808dbf5456ecd9caa83fed
Chris Smothers <chris.smothers@gmail.com>
2021-03-14 13:53:33 -0400

    update entity tree view

--- src/main/datalog_console/components/entity.cljs
+++ src/main/datalog_console/components/entity.cljs
@@ -10,7 +10,6 @@
                                 :db/cardinality :db.cardinality/many}
                :employer/person {:db/valueType :db.type/ref
                                  :db/cardinality :db.cardinality/one}})]
-
     (d/transact! conn [{:db/id -1
                         :name "A"
                         :item/size 2
@@ -33,58 +32,57 @@
     conn))
 
 (defn entity? [v]
-  (some? false #_(try
-           (:db/id (first v))
-           (catch js/Error e false))))
-
-
-
+  (try
+    (not (nil? (:db/id v)))
+    (catch js/Error e false)))
 
 (defn entity-tree-av [[a v]]
-  [:div {:class "flex justify w-full hover:bg-gray-200"}
-   [:span {:class "w-1/2 pl-2"} (str a)]
-   [:span {:class "w-1/2 pl-4"} (str v)]])
-
+  (let [entity? (entity? (if (set? v) (first v) v))]
+    [:div {:class "flex w-full odd:bg-gray-100"}
+     [:span
+      {:title (str a)
+       :class "w-1/2 pl-2 line-clamp-1"}
+      (str a)]
+     [:span
+      {:title (str v)
+       :class "w-1/2 pl-4 line-clamp-1"}
+      (if (and entity? (set? v))
+        (str "[" (count v) " item" (when (< 1 (count v)) "s") "]")
+        (str v))]]))
 
 (defn make-keyword-reverse-ref [kw]
   (keyword (str (namespace kw) "/_" (name kw))))
 
-
-
+(defn reverse-refs [entity]
+  (let [rev-ref-attrs-and-eids (d/q '[:find ?ref-attr ?e
+                                          :in $ [?ref-attr ...] ?ref-id
+                                          :where [?e ?ref-attr ?ref-id]]
+                                        @conn
+                                        (for [[attr props] (:schema @conn)
+                                              :when (= :db.type/ref (:db/valueType props))]
+                                          attr)
+                                        (:db/id entity))
+        rev-ref-eids-grouped-by-attr (group-by first rev-ref-attrs-and-eids)
+        rev-ref-entities-grouped-by-attr (reduce-kv (fn [acc k v]
+                                                          (conj acc [(make-keyword-reverse-ref  k)
+                                                                     (set (map (fn [[_ eid]] (d/entity @conn eid)) v))]))
+                                                        [] rev-ref-eids-grouped-by-attr)]
+    rev-ref-entities-grouped-by-attr))
 
 (defn entity-tree [entity]
-  [:div {:class (str "mt-4")}
-   (when-let [db-id (:db/id entity)]
-     (let [reverse-lookups (d/q '[:find ?e ?ref
-                                  :in $ [?ref ...] ?ref-id
-                                  :where [?e ?ref ?ref-id]]
-                                @conn
-                                (for [[attr props] (:schema @conn)
-                                      :when (= :db.type/ref (:db/valueType props))]
-                                  attr)
-                                db-id)
-           group-reverse-lookups (group-by second reverse-lookups)
-           counted-reverse-refs (reduce-kv (fn [vect k v]
-                                             (conj vect [(make-keyword-reverse-ref  k)
-                                                         (set (map (fn [[eid]] (d/entity @conn eid)) v))
-                                                         true
-                                                         #_(str "[ " (count v) " items ]")]))
-                                           []
-                                           group-reverse-lookups)
-           entity-attrs (reduce conj (vec (seq entity)) counted-reverse-refs)]
-       [:div
-        [:div
-         [:div {:class "flex justify w-full font-bold"}
-          [:span {:class "w-1/2 pl-2"} "Attribute"]
-          [:span {:class "w-1/2 pl-4"} "Value"]]
-         [entity-tree-av [:db/id db-id]]]
-        (for [[a :as av] entity-attrs #_(seq entity)]
-          ^{:key (pr-str a)} [entity-tree-av av])]))])
-
+  [:div {:class "mt-4"}
+   [:div
+    [:div {:class "flex justify w-full font-bold"}
+     [:span {:class "w-1/2 pl-2"} "Attribute"]
+     [:span {:class "w-1/2 pl-4"} "Value"]]
+    [entity-tree-av [:db/id (:db/id entity)]]
+    (for [[a :as av] (into
+                      (vec entity)
+                      (reverse-refs entity))]
+      ^{:key (str a)} [entity-tree-av av])]])
 
 (defn entity []
   (let [lookup (r/atom "")
-        entity (r/atom nil)]
     (fn []
       [:div {:class "w-full h-full"}
        [:label [:p {:class "font-bold"} "Entity lookup"]
