87b6cef5530c55858c86c1aaf7ad90dc3bcc475e
Chris Smothers <chris.smothers@gmail.com>
2021-03-25 11:25:32 -0400

    refactor formatters

--- src/dev/datalog_console/workspaces/chrome/formatter_cards.cljs
+++ src/dev/datalog_console/workspaces/chrome/formatter_cards.cljs
@@ -2,37 +2,12 @@
   (:require [nubank.workspaces.core :as ws]
             [nubank.workspaces.card-types.react :as ct.react]
             [datascript.core :as d]
-            [datascript.impl.entity :as de]
-            [clojure.string :as string]
-            [devtools.formatters.markup :as m]
-            [devtools.formatters.templating :refer [render-markup]]
-            [devtools.protocols :refer [IFormat]]
-            [devtools.core :as devtools]
             [clojure.test :refer [is]]
-            ;; [datalog-console.chrome.formatters :as f]
-            [datalog-console.components.entity :as c.entity]
+            [datalog-console.chrome.formatters]
             [datalog-console.workspaces.entity-cards :refer [conn]]))
-
-(devtools/set-pref! :disable-advanced-mode-check true)
-(devtools/install! [:formatters :hints])
 
 (defn element [name props & children]
   (apply js/React.createElement name (clj->js props) children))
-
-;; (f/install)
-(extend-type de/Entity
-  IFormat
-  (-header [entity] (render-markup [["span" "color:white;background-color:#999;padding:0px 4px;"] (str "Entity: " (select-keys entity [:db/id]))]))
-  (-has-body [_] true)
-  (-body [entity] (render-markup
-                   (into [[:table "border:1px solid black;"]]
-                         (for [[k v] (c.entity/entity->rows entity)]
-                           [[:tr]
-                            [[:td] (pr-str k)]
-                            [[:td] (cond
-                                     (c.entity/entity? v) #js ["object" #js {"object" v}]
-                                     (and (set? v) (c.entity/entity? (first v))) #js ["object" #js {"object" v}]
-                                     :else (pr-str v))]])))))
 
 (ws/defcard chrome-formatters-card
   (do
@@ -42,11 +17,8 @@
     (ct.react/react-card
      (element "div" {:className "font-black"} "Open the chrome console"))))
 
-
 (deftype SampleType [a b])
-
 (defrecord SampleRecord [c d e])
-
 (ws/deftest test-cljs$lang$type
   (is (= (.-cljs$lang$type SampleType) true))
   (is (= (pr-str SampleType) "datalog-console.workspaces.chrome.formatter-cards/SampleType"))
---
+++ src/main/datalog_console/chrome/formatters.cljs
@@ -0,0 +1,25 @@
+(ns datalog-console.chrome.formatters
+  (:require [datascript.impl.entity :as de]
+            [datalog-console.components.entity :as c.entity]
+            [devtools.core :as devtools]
+            [devtools.formatters.templating :refer [render-markup]]
+            [devtools.protocols :refer [IFormat]]))
+
+(devtools/set-pref! :disable-advanced-mode-check true)
+(devtools/install! [:formatters :hints])
+
+(extend-type de/Entity
+  IFormat
+  (-header [entity] (render-markup
+                     [[:span "color:white;background-color:#999;padding:0px 4px;"]
+                      (str "Entity: " (select-keys entity [:db/id]))]))
+  (-has-body [_] true)
+  (-body [entity] (render-markup
+                   (into [[:table "border:1px solid black;"]]
+                         (for [[k v] (c.entity/entity->rows entity)]
+                           [[:tr]
+                            [[:td] (pr-str k)]
+                            [[:td] (cond
+                                     (c.entity/entity? v) #js ["object" #js {"object" v}]
+                                     (and (set? v) (c.entity/entity? (first v))) #js ["object" #js {"object" v}]
+                                     :else (pr-str v))]])))))
