fb628125ce3c26ce9cb6e4e8bca5cce190f184cf
Chris Smothers <chris.smothers@gmail.com>
2021-03-26 13:46:15 -0400

    refactor and style schema

--- src/dev/datalog_console/workspaces/schema_cards.cljs
+++ src/dev/datalog_console/workspaces/schema_cards.cljs
@@ -3,12 +3,48 @@
             [nubank.workspaces.model :as wsm]
             [nubank.workspaces.card-types.react :as ct.react]
             [reagent.core :as reagent]
-            [datalog-console.components.schema :as c.schema]))
+            [datalog-console.components.schema :as c.schema]
+            [datascript.core :as d]))
+
+(def conn
+  (let [conn (d/create-conn
+              {:name {:db/unique :db.unique/identity}
+               :person/parents {:db/valueType :db.type/ref
+                                :db/cardinality :db.cardinality/many}
+               :person/friends {:db/valueType :db.type/ref
+                                :db/cardinality :db.cardinality/many}
+               :employer/person {:db/valueType :db.type/ref
+                                 :db/cardinality :db.cardinality/one}})]
+    (d/transact! conn [{:db/id -1
+                        :name "A"
+                        :description "I'm baby truffaut craft beer cold-pressed sartorial listicle aesthetic normcore edison bulb XOXO seitan celiac. Blog irony succulents synth shabby chic. Health goth hexagon semiotics wolf jean shorts narwhal, intelligentsia hell of hot chicken. La croix pour-over coloring book wayfarers austin, asymmetrical whatever messenger bag four dollar toast activated charcoal vice banjo vegan portland. Twee selfies chillwave normcore 3 wolf moon af coloring book flexitarian plaid poke mlkshk pitchfork bicycle rights. Franzen gluten-free lyft snackwave meditation man braid, tousled shabby chic banjo. Selfies tote bag chicharrones, hammock fam umami woke typewriter poutine fanny pack artisan copper mug."
+                        :employer/person -2
+                        :item/size 2
+                        :item/age 3
+                        :item/with-a-very-very-very-long-name 3
+                        :item/thingy 4}
+                       {:db/id -2
+                        :name "B"
+                        :person/parents [-1 -3]}
+                       {:db/id -3
+                        :name "C"}
+                       {:db/id -4
+                        :name "D"
+                        :description 1
+                        :person/parents [-1 -3]}
+                       {:db/id -5
+                        :name 1
+                        :person/friends [-1]}
+                       {:db/id -6
+                        :name (js/Date.)
+                        :employer/person -1}])
+    conn))
 
 (ws/defcard schema-card
-  {::wsm/align {:flex 1}}
+  {::wsm/align {:flex 1}
+   ::wsm/node-props {:style {:overflow "hidden" :padding 0}}}
   (ct.react/react-card
-   (reagent/as-element [c.schema/schema])))
+   (reagent/as-element [c.schema/schema @conn])))
 
 
 
--- src/main/datalog_console/components/schema.cljs
+++ src/main/datalog_console/components/schema.cljs
@@ -1,184 +1,65 @@
 (ns datalog-console.components.schema
   (:require [datascript.core :as d]
-            [reagent.core :as r]))
+            [reagent.core :as r]
+            [datalog-console.components.tree-table :as c.tree-table]))
 
-(defonce conn
-  (let [conn (d/create-conn
-              {:parent {:db/valueType :db.type/ref
-                        :db/cardinality :db.cardinality/one
-                        :db/doc "the parent"}
-               :child {:db/valueType :db.type/ref
-                       :db/cardinality :db.cardinality/one
-                       :db/doc "the child"}
-               :employer/person {:db/valueType :db.type/ref
-                                 :db/cardinality :db.cardinality/one
-                                 :db/doc "the employer"}
-               :employer/parent {:db/valueType :db.type/ref
-                                 :db/cardinality :db.cardinality/one
-                                 :db/doc "the parent employer"}
-               :test.employer/person {:db/valueType :db.type/ref
-                                      :db/cardinality :db.cardinality/one
-                                      :db/doc "the test employer"}})]
-    (d/transact! conn [{:db/id -1
-                        :name "A"
-                        :age 1
-                        :item/size 2
-                        :item/age 3
-                        :item/thingy 4}
-                       {:db/id -2
-                        :name "B"
-                        :parent -1}
-                       {:db/id -3
-                        :name 3
-                        :parent -1}
-                       {:db/id -4
-                        :name "D"
-                        :person/parents [-1 -3]}
-                       {:db/id -5
-                        :name/parent "C"
-                        :parent -1
-                        :item/age "20"
-                        :item/thingy 1}])
-    conn))
+(defn deep-merge [v & vs]
+  (letfn [(rec-merge [v1 v2]
+            (if (and (map? v1) (map? v2))
+              (merge-with deep-merge v1 v2)
+              v2))]
+    (when (some identity vs)
+      (reduce #(rec-merge %1 %2) v vs))))
 
-(def conn2
-  (d/create-conn
-   {:country {:db/valueType :db.type/ref
-              :db/cardinality :db.cardinality/one
-              :db/doc "the parent"}
-    :city {:db/valueType :db.type/ref
-           :db/cardinality :db.cardinality/one
-           :db/doc "the child"}}))
+(defn v->type [v]
+  (cond
+    (string? v) :db.type/string
+    (number? v) :db.type/long
+    :else (pr-str (type v))))
 
+(defn infer-schema [v]
+  {:db/valueType  {::inferred {(v->type v) (v->type v)}}})
 
-(def db-list {:idb conn2 :mem conn})
+(defn expandable-row? [[a v]]
+  (and 
+   (coll? v)
+   (not (set? v)) 
+   (not (::inferred v))))
 
+(defn expand-row [[a v]]
+  (sort v))
 
-;; Utilities
+(defn render-col [col]
+  (cond
+    (expandable-row? [nil col]) nil
+    (::inferred col) (let [a (str (set (keys (::inferred col))))]
+                       [:div {:title a :class "w-full"}
+                        [:span {:title "This database attribute is inferred based on data in the DB. It is not hardcoded or enforced in any way."}
+                         "ℹ️ "]
+                        a])
+    :else (str col)))
 
-(defn schema-vals [schema]
-  (reduce-kv (fn [m k v]
-               (assoc m (keyword (name k)) v))
-             {}
-             schema))
-
-
-(defn inferred-schema-types [v]
-  (let [many? (coll? v)
-        value #(cond
-                 (string? %) :db.type/string
-                 (number? %) :db.type/long
-                 :else "Unknown")
-        valueType (set (map value v))]
-    {:valueType (first valueType)
-     :cardinality (if many? :db.cardinality/many :db.cardinality/one)}))
-
-
-(defn infer-schema-type [v]
-  (let [many? (coll? v)
-        value #(cond
-                 (string? %) :db.type/string
-                 (number? %) :db.type/long
-                 :else "Unknown")
-        valueType (if many?
-                    (set (map value v)) ; We could check here to see if there is more than one type
-                    (value  v))]
-    {:valueType  #{valueType :inferred}
-     :cardinality #{(if many? :db.cardinality/many :db.cardinality/one) :inferred}}))
-
-(defn merge-schemas [infered-schema real-schema]
-  (reduce-kv (fn [acc k v] (update acc k merge v)) infered-schema real-schema))
-
-
-(defn agg-attrs-by-ns [entity]
+(defn coll->schema [coll a-fn v-fn]
   (reduce
-   (fn [acc [a v]]
-     (assoc-in acc [(namespace a) (name a)] v))
-   {} entity))
+   (fn [acc x]
+     (deep-merge
+      acc
+      {(str ":" (when-let [n (namespace (a-fn x))]
+                  (str n "/")))
+       {(name (a-fn x)) 
+        (v-fn x)}}))
+   {} coll))
 
-
-(defn generate-schema-datastructure [on-write? schema]
-  (let [convert-vals #(if on-write? (schema-vals %) (infer-schema-type %) #_(inferred-schema-types %))]
-    (->> (agg-attrs-by-ns schema)
-         (map (fn [[k v]]
-                (let [extract-vals #(reduce-kv (fn [m k v]
-                                                 (assoc m k (convert-vals v))) {} %)]
-                  (if k
-                    {(str  k "/") (extract-vals v)}
-                    (extract-vals v))))))))
-
-
-
-;; Views
-
-(defn schema-tree-av [[a v]]
-  [:div
-   [:div {:class "flex justify w-full hover:bg-gray-200"}
-    [:span {:class "w-1/2 pl-2"}
-     (str (name a))]
-    [:span {:class "w-1/2 pl-4"}
-     (if (contains? v :inferred)
-       [:<> (str (first (disj v :inferred))) [:span {:class "pl-2 text-gray-400"} "ℹ️"]]
-       (str v))]]])
-
-
-(defn schema-tree-ns [[a-ns av-pairs-in-ns] & nested?]
-  (let [open? (r/atom false)]
-    (fn []
-      (if (clojure.string/ends-with? a-ns "/")
-        [:div
-         [:div {:class "flex"}
-          [:button {:class "pr-1 focus:outline-none"
-                    :on-click #(reset! open? (not @open?))}
-           (if @open? "▼" "▶")]
-          [:p {:class "w-1/2 pl-2 w-full hover:bg-gray-200 mt-2 "}
-           (str ":" a-ns)]]
-         (when @open?
-           [:div {:class "pl-2"}
-            (for [[a-ns :as x] av-pairs-in-ns]
-              ^{:key (pr-str a-ns)} [schema-tree-ns x true])])]
-        [:div
-         (let [doc (:doc av-pairs-in-ns)
-               av-pairs-in-ns (if doc (dissoc av-pairs-in-ns :doc) av-pairs-in-ns)]
-           [:div {:class "flex"}
-            [:button {:class "pr-1 focus:outline-none"
-                      :on-click #(reset! open? (not @open?))}
-             (if @open? "▼" "▶")]
-            [:div {:class (str "flex justify w-full hover:bg-gray-200" (when (not nested?) " mt-2"))}
-             [:span {:class "w-1/2 pl-2"}
-              (if nested? (str a-ns) (str ":" a-ns))]
-             [:span {:class "w-1/2 pl-4"}
-              (str doc)]]])
-         (when @open?
-           [:div {:class "pl-3"}
-            (for [[a :as av] (seq av-pairs-in-ns)]
-              ^{:key (pr-str a)} [schema-tree-av av])])]))))
-
-
-(defn schema-tree [schema]
-  [:div
-   (for [[a-ns :as x] schema]
-     ^{:key (pr-str a-ns)} [schema-tree-ns x])])
-
-
-(defn schema []
-  (let [db-name (r/atom :mem)]
-    (fn []
-      (let [real-schema (reduce merge (generate-schema-datastructure true (:schema @(@db-name db-list))))
-            inferred-schema (reduce merge (map (fn [d]
-                                                 {(name (:a d)) (infer-schema-type (:v d))})
-                                               (d/datoms @(@db-name db-list) :eavt)))
-            merged-schemas (merge-schemas inferred-schema real-schema)]
-        [:div
-         [:label {:for "database"} "DB"]
-         [:select {:class "border rounded ml-2 px-2 py-1"
-                   :name "database"
-                   :on-change #(reset! db-name (cljs.reader/read-string (.-value (.-target %))))}
-          (for [d (keys db-list)]
-            ^{:key (str d)} [:option {:value (pr-str d)} (pr-str d)])]
-         [:div {:class "w-full h-full mt-4"}
-            [:div {:class "flex justify w-full font-bold"}
-             [:span {:class "w-1/2 pl-2"} "Attribute"]
-             [:span {:class "w-1/2 pl-4"} "Value"]]
-            [schema-tree merged-schemas]]]))))
-
+(defn schema [db]
+  (when db
+    (let [real-schema (coll->schema (:schema db) first last)
+          inferred-schema (coll->schema (d/datoms db :eavt) :a #(infer-schema (:v %)))
+          merged-schemas (deep-merge inferred-schema real-schema)]
+      [:div {:class "w-full h-full overflow-auto pb-5"}
+       [c.tree-table/tree-table
+        {:caption "schema"
+         :head-row ["Attribute", "Value"]
+         :rows (sort merged-schemas)
+         :expandable-row? expandable-row?
+         :expand-row expand-row
+         :render-col render-col}]])))
