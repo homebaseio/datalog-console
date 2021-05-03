b38d92f2efee4391167918f5b7abe5610356984a
Wade Dominic <wade@lambdaforge.io>
2021-05-03 16:55:03 +0200

    add placeholder scripts for handling panel events

--- shells/chrome/devtools-init.js
+++ shells/chrome/devtools-init.js
@@ -1,8 +1,30 @@
+/*
+ This code can be used to trigger a stop and start on subscriptions to the database
+*/
+
+var port = chrome.runtime.connect({name: "example"})
+
+function handleShown() {
+  console.log("panel is being shown");
+// port.postMessage({example: "message"});
+}
+
+function handleHidden() {
+  console.log("panel is being hidden");
+}
+
+/*
+ end note
+*/
+
+
+
 chrome.devtools.panels.create(
   "Datalog DB",
   "",
   "inspect-panel.html",
   function (panel) {
-    console.log("panel initialized", panel);
+    panel.onShown.addListener(handleShown);
+    panel.onHidden.addListener(handleHidden);
   }
 );
