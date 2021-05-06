588ac974d036cb67b25848c8fefeeb6e5137d9c3
Wade Dominic <wade@lambdaforge.io>
2021-05-06 17:31:51 +0200

    add entity scanner to console

--- src/main/datalog_console/client.cljs
+++ src/main/datalog_console/client.cljs
@@ -3,6 +3,7 @@
             [reagent.core :as r]
             [datalog-console.components.schema :as c.schema]
             [datalog-console.components.entity :as c.entity]
+            [datalog-console.components.entity-scanner :as c.entity-scanner]
             [datascript.core :as d]
             [goog.object :as gobj]
             [cljs.reader]))
@@ -38,9 +39,12 @@
        [:div {:class "w-80 border-r stack-small pt-2"}
         [:h2 {:class "pl-1 text-xl border-b flex center"} "Schema Viewer"]
         [c.schema/schema @rconn]]
+       [:div {:class "w-80 border-r"}
+        [:h2 {:class "px-1 text-xl border-b pt-2"} "Entity Scanner"]
+        [c.entity-scanner/entity-scan @rconn entity-lookup-ratom]]
        [:div {:class "flex-auto stack-small"}
         [:h2 {:class "px-1 text-xl border-b pt-2"} "Entity Inspector"]
-        [c.entity/entity @rconn]]
+        [c.entity/entity @rconn entity-lookup-ratom]]
        [:button
         {:class "absolute top-2 right-1 py-1 px-2 rounded bg-gray-200 border shadow-hard btn-border"
          :on-click (fn []
