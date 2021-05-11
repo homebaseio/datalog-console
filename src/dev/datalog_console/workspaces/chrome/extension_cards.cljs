d95716a65ef6dc01817a6834239301d33b8f19e8
groundedSAGE <wade@lambdaforge.io>
2021-05-11 18:28:15 +0200

    fix namespaceing in extension cards

--- src/dev/datalog_console/workspaces/chrome/extension_cards.cljs
+++ src/dev/datalog_console/workspaces/chrome/extension_cards.cljs
@@ -10,15 +10,15 @@
   ;; TODO: consider passing in a map to allow passing in extra information without creating breaking changes.
   ;; One example could be passing in an atom containing a register of the databases.
   ;; We could watch this atom and based on databases being registered or de-registered post a message of the database existance.
-  (js/document.documentElement.setAttribute "__datalog-inspect-remote-installed__" true)
+  (js/document.documentElement.setAttribute "__datalog-console-remote-installed__" true)
   (.addEventListener js/window "message"
                      (fn [event]
-                       (when-let [devtool-message (gobj/getValueByKeys event "data" "devtool-message")]
+                       (when-let [devtool-message (gobj/getValueByKeys event "data" ":datalog-console.client/devtool-message")]
                          (let [msg-type (:type (cljs.reader/read-string devtool-message))]
                            (case msg-type
 
                              :datalog-console.client/request-whole-database-as-string
-                             (.postMessage js/window #js {:datalog-remote-message (pr-str @conn)} "*")
+                             (.postMessage js/window #js {":datalog-console.remote/remote-message" (pr-str @conn)} "*")
 
                              nil))))))
 
