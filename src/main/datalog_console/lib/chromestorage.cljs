fc4cf9ba968e995a67aae1816670adb78a81451d
groundedSAGE <wade@lambdaforge.io>
2021-06-07 18:50:55 +0200

    fix: use chrome.storage for saved queries (#17)

---
+++ src/main/datalog_console/lib/chromestorage.cljs
@@ -0,0 +1,12 @@
+(ns datalog-console.lib.chromestorage
+  (:require [goog.object :as gobj]))
+
+(defn set-item!
+  "Set `key` in browser's extension storage to `val`. Call optional function `f` when done."
+  [k v]
+  (js/chrome.storage.local.set (clj->js {(str k) v}) nil))
+
+(defn get-item
+  "Returns value of `key` from browser's extension storage. Call optional function `f` when done."
+  [key cb]
+  (js/chrome.storage.local.get (str key) cb))
