1391e0c25268db93d6fed5aecc0c3490f7e4620d
groundedSAGE <wade@lambdaforge.io>
2021-06-15 18:38:08 +0200

    update handlers

--- src/main/datalog_console/integrations/datascript.cljs
+++ src/main/datalog_console/integrations/datascript.cljs
@@ -1,6 +1,14 @@
 (ns datalog-console.integrations.datascript
   (:require [goog.object :as gobj]
-            [cljs.reader]))
+            [cljs.reader]
+            [datascript.core :as d]))
+
+(defn transact-from-remote! [conn transact-str]
+  (try
+    (d/transact conn (cljs.reader/read-string transact-str))
+    :success
+    (catch js/Error e {:error (goog.object/get e "message")})))
+
 
 (defn enable! 
   "Takes a [datascript](https://github.com/tonsky/datascript) database connection atom. Adds message handlers for a remote datalog-console process to communicate with. E.g. the datalog-console browser [extension](https://chrome.google.com/webstore/detail/datalog-console/cfgbajnnabfanfdkhpdhndegpmepnlmb?hl=en)."
@@ -14,5 +22,9 @@
 
                              :datalog-console.client/request-whole-database-as-string
                              (.postMessage js/window #js {":datalog-console.remote/remote-message" (pr-str @conn)} "*")
+
+                             :datalog-console.client/transact!
+                             (let [transact-result (transact-from-remote! conn (:data (cljs.reader/read-string devtool-message)))]
+                               (.postMessage js/window #js {":datalog-console.remote/remote-message" (pr-str transact-result)} "*"))
 
                              nil))))))
