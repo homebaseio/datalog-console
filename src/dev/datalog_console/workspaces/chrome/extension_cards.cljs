05974e1f6fcf9700e8b2532db2c2f9ac6fb1819c
groundedSAGE <wade@lambdaforge.io>
2021-06-15 18:37:54 +0200

    fixes from merge from main

--- src/dev/datalog_console/workspaces/chrome/extension_cards.cljs
+++ src/dev/datalog_console/workspaces/chrome/extension_cards.cljs
@@ -2,38 +2,8 @@
   {:no-doc true}
   (:require [nubank.workspaces.core :as ws]
             [cljs.reader]
-            [datascript.core :as d]
             [nubank.workspaces.card-types.react :as ct.react]
-            [datalog-console.workspaces.workspace-db-conn :refer [conn]]))
-
-(defn transact-from-remote! [conn transact-str]
-  (try
-    (d/transact conn (cljs.reader/read-string transact-str))
-    :success
-    (catch js/Error e {:error (goog.object/get e "message")})))
-
-(defn enable-remote-database-inspection []
-  ;; TODO: consider passing in a map to allow passing in extra information without creating breaking changes.
-  ;; One example could be passing in an atom containing a register of the databases.
-  ;; We could watch this atom and based on databases being registered or de-registered post a message of the database existance.
-  (js/document.documentElement.setAttribute "__datalog-console-remote-installed__" true)
-  (.addEventListener js/window "message"
-                     (fn [event]
-                       (when-let [devtool-message (gobj/getValueByKeys event "data" ":datalog-console.client/devtool-message")]
-                         (let [msg-type (:type (cljs.reader/read-string devtool-message))]
-                           (case msg-type
-
-                             :datalog-console.client/request-whole-database-as-string
-                             (.postMessage js/window #js {":datalog-console.remote/remote-message" (pr-str @conn)} "*")
-
-                             :datalog-console.client/transact!
-                             (let [transact-result (transact-from-remote! conn (:data (cljs.reader/read-string devtool-message)))]
-                               (.postMessage js/window #js {":datalog-console.remote/remote-message" (pr-str transact-result)} "*"))
-                             nil))))))
-
-
-(enable-remote-database-inspection)
-            [datalog-console.workspaces.entity-cards :refer [conn]]
+            [datalog-console.workspaces.workspace-db-conn :refer [conn]]
             [datalog-console.integrations.datascript :as integrations]))
 
 (integrations/enable! {:conn conn})
